1629ef22410dd5778e8cc0dbe8c467cd7f3e4f88
Tristan Larson <telarson@ksu.edu>
2020-01-29 15:35:55 -0600

    Angry Chicken and Rustlers Ribs classes written and tested.

--- Data/AngryChicken.cs
+++ Data/AngryChicken.cs
@@ -4,7 +4,68 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class AngryChicken
+    /// <summary>
+    /// Class representing the Angry Chicken entree
+    /// </summary>
+    public class AngryChicken
     {
+        /// <summary>
+        /// The price of an Angry Chicken
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99;
+            }
+        }
+
+        /// <summary>
+        /// The caloric content of the Angry Chicken
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+
+        private bool bread = true;
+        /// <summary>
+        /// Bool representing if the Angry Chicken should include bread
+        /// </summary>
+        public bool Bread
+        {
+            get
+            {
+                return bread;
+            }
+
+            set
+            {
+                bread = value;
+            }
+        }
+
+        /// <summary>
+        /// Bool representing if pickle is included with the Angry Chicken
+        /// </summary>
+        public bool Pickle { get; set; } = true;
+
+        /// <summary>
+        /// List to store special instructions for the Angry Chicken
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+
+                if (!Bread) { instructions.Add("hold bread"); }
+                if (!Pickle) { instructions.Add("hold pickle"); }
+                return instructions;
+            }
+        }
     }
 }
--- Data/RustlersRibs.cs
+++ Data/RustlersRibs.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class RustlersRibs
+    /// <summary>
+    /// Class representing the Rustlers Ribs menu entree
+    /// </summary>
+    public class RustlersRibs
     {
+        /// <summary>
+        /// The price of the rustlers ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The caloric content of the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+
+        /// <summary>
+        /// List to store special instructions for the Ribs
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                return instructions;
+            }
+        }
+
     }
 }
