d4d54ac30a8dbb973aae0252701a1cd8651ea038
Tristan Larson <telarson@ksu.edu>
2020-01-30 00:03:33 -0600

    DakotaDoubleBurger class written and tested

--- Data/DakotaDoubleBurger.cs
+++ Data/DakotaDoubleBurger.cs
@@ -4,7 +4,81 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class DakotaDoubleBurger
+    /// <summary>
+    /// Class for the DakotaDoubleBurger
+    /// </summary>
+    public class DakotaDoubleBurger
     {
+        /// <summary>
+        /// Price of the DakotaDoubleBurger
+        /// </summary>
+        public double Price { get; } = 5.20;
+
+        /// <summary>
+        /// Calorie content of the DakotaDoubleBurger
+        /// </summary>
+        public uint Calories { get; } = 464;
+
+        /// <summary>
+        /// Boolean flag for a Bun on the DakotaDoubleBurger
+        /// </summary>
+        public bool Bun { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Ketchup on the DakotaDoubleBurger
+        /// </summary>
+        public bool Ketchup { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Mustard on the DakotaDoubleBurger
+        /// </summary>
+        public bool Mustard { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Pickle on the DakotaDoubleBurger
+        /// </summary>
+        public bool Pickle { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Cheese on the DakotaDoubleBurger
+        /// </summary>
+        public bool Cheese { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Tomato on the DakotaDoubleBurger
+        /// </summary>
+        public bool Tomato { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Lettuce on the DakotaDoubleBurger
+        /// </summary>
+        public bool Lettuce { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Mayo on the DakotaDoubleBurger
+        /// </summary>
+        public bool Mayo { get; set; } = true;
+
+        /// <summary>
+        /// Special instructions for the DakotaDoubleBurger
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+
+                if (!Bun) { instructions.Add("hold bun"); }
+                if (!Ketchup) { instructions.Add("hold ketchup"); }
+                if (!Mustard) { instructions.Add("hold mustard"); }
+                if (!Cheese) { instructions.Add("hold cheese"); }
+                if (!Pickle) { instructions.Add("hold pickle"); }
+                if (!Tomato) { instructions.Add("hold tomato"); }
+                if (!Lettuce) { instructions.Add("hold lettuce"); }
+                if (!Mayo) { instructions.Add("hold mayo"); }
+
+                return instructions;
+            }
+        }
     }
 }
