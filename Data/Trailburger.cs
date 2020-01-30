d64bcaa5e88041e0b24122a882431ef2c58f4f44
Tristan Larson <telarson@ksu.edu>
2020-01-29 21:47:17 -0600

    Trailbuger written and tested

--- Data/Trailburger.cs
+++ Data/Trailburger.cs
@@ -4,7 +4,63 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class Trailburger
+    /// <summary>
+    /// Class representation of the Trailburger entree
+    /// </summary>
+    public class Trailburger
     {
+        /// <summary>
+        /// Price for the Trailburger entree
+        /// </summary>
+        public double Price { get; } = 4.50;
+
+        /// <summary>
+        /// Calorie content for the Trailburger entree
+        /// </summary>
+        public uint Calories { get; } = 288;
+
+        /// <summary>
+        /// Boolean flag for a Bun on the Trailburger
+        /// </summary>
+        public bool Bun { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Ketchup on the Trailburger
+        /// </summary>
+        public bool Ketchup { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Mustard on the Trailburger
+        /// </summary>
+        public bool Mustard { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Pickle on the Trailburger
+        /// </summary>
+        public bool Pickle { get; set; } = true;
+
+        /// <summary>
+        /// Boolean flag for Cheese on the Trailburger
+        /// </summary>
+        public bool Cheese { get; set; } = true;
+
+        /// <summary>
+        /// Special instructions for the Trailburger
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+
+                if (!Bun) { instructions.Add("hold bun"); }
+                if (!Ketchup) { instructions.Add("hold ketchup"); }
+                if(!Mustard) { instructions.Add("hold mustard"); }
+                if(!Cheese) { instructions.Add("hold cheese"); }
+                if (!Pickle) { instructions.Add("hold pickle"); }
+
+                return instructions;
+            }
+        }
     }
 }
