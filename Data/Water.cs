899a944a5e4b7bd146129ca4d70b11eef14e1e64
Tristan Larson <telarson@ksu.edu>
2020-02-13 11:48:43 -0600

    Added and tested the class Water.cs

---
+++ Data/Water.cs
@@ -0,0 +1,51 @@
+﻿/* Water.cs
+ * Author: Tristan Larson
+ * Class for the Water drink,
+ * inherits from Drink.cs
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class Water : Drink
+    {
+        /// <summary>
+        /// Bool for adding a lemon to the water
+        /// </summary>
+        public bool Lemon { get; set; } = false;
+
+        /// <summary>
+        /// Property that returns the price of a water
+        /// </summary>
+        public override double Price
+        {
+            get { return 0.12;}
+        }
+
+        /// <summary>
+        /// Property that returns the calories in a water
+        /// </summary>
+        public override uint Calories
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// Property that returns the list of special instructions for the water
+        /// </summary>
+        public override List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+
+                if (!Ice) { instructions.Add("Hold Ice"); }
+                if (Lemon) { instructions.Add("Add Lemon"); }
+
+                return instructions;
+            }
+        }
+    }
+}
