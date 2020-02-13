a2931ad2458cb328dd5611ccc78a26c0060a0c92
Tristan Larson <telarson@ksu.edu>
2020-02-13 11:12:43 -0600

    Added Enum SodaFlavor.cs

---
+++ Data/SodaFlavor.cs
@@ -0,0 +1,22 @@
+﻿/* SodaFlavor.cs
+ * Author: Tristan Larson
+ * Enum for the different flavors of soda
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Enum for the flavors of soda.
+    /// </summary>
+    public enum SodaFlavor
+    {
+        CreamSoda,
+        OrangeSoda,
+        Sarsaparilla,
+        BirchBeer,
+        RootBeer
+    }
+}
