570e313c0ba4e4a932bc47c5aa950033f06e37e1
Tristan Larson <telarson@ksu.edu>
2020-02-23 13:07:37 -0600

    Rigged event handlers to all button click events in OrderControl.

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*OrderControl.cs
+ * Author: Tristan Larson
+ * Contains event handlers and logic for the buttons in the OrderControl.
+ */
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -22,9 +26,27 @@ namespace PointOfSale
         public OrderControl()
         {
             InitializeComponent();
+
+            //Hook up events with handlers for entree buttons.
             AddCowpokeChiliButton.Click += OnAddCowpokeChiliButtonClicked;
-            AddDakotaDoubleBurgerButton.Click += AddDakotaDoubleBurgerButtonClicked;
-            AddAngryChickenButton.Click += AddAngryChickenButtonClicked;
+            AddDakotaDoubleBurgerButton.Click += OnAddDakotaDoubleBurgerButtonClicked;
+            AddAngryChickenButton.Click += OnAddAngryChickenButtonClicked;
+            AddRustlersRibsButton.Click += OnAddRustlersRibsButtonClicked;
+            AddPecosPulledPorkButton.Click += OnAddPecosPulledPorkButtonClicked;
+            AddTrailBurgerButton.Click += OnAddTrailBurgerButtonClicked;
+            AddTexasTripleBurgerButton.Click += OnAddTexasTripleBurgerButtonClicked;
+
+            //Hook up events with handlers for side buttons.
+            AddChiliCheeseFries.Click += OnAddChiliCheeseFriesButtonClicked;
+            AddCornDodgersButton.Click += OnAddCornDodgersButtonClicked;
+            AddPanDeCampoButton.Click += OnAddPanDeCampoButtonClicked;
+            AddBakedBeansButton.Click += OnAddBakedBeansButtonClicked;
+
+            //Hook up events with handlers for drink buttons.
+            AddJerkedSodaButton.Click += OnAddJerkedSodaButtonClicked;
+            AddTexasTeaButton.Click += OnAddTexasTeaButtonClicked;
+            AddCowboyCoffeeButton.Click += OnAddCowboyCoffeeButtonClicked;
+            AddWaterButton.Click += OnAddWaterButtonClicked;
         }
 
         /// <summary>
@@ -38,13 +60,53 @@ namespace PointOfSale
         }
 
         /// <summary>
+        /// Event handler for AddRustlersRibsButton click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddRustlersRibsButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new RustlersRibs());
+        }
+
+        /// <summary>
+        /// Event handler for AddPecosPulledPorkButton Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddPecosPulledPorkButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new PecosPulledPork());
+        }
+
+        /// <summary>
+        /// Event handler for AddTrailBurgerButton Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddTrailBurgerButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new TrailBurger());
+        }
+
+        /// <summary>
         /// Event handler for the AddDakotaDoubleBurger button.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void AddDakotaDoubleBurgerButtonClicked(object sender, RoutedEventArgs e)
+        void OnAddDakotaDoubleBurgerButtonClicked(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new DakotaDoubleBurger());
+        }
+
+        /// <summary>
+        /// Event handler for the AddDakotaDoubleBurger button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddTexasTripleBurgerButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new TexasTripleBurger());
         }
 
         /// <summary>
@@ -52,9 +114,90 @@ namespace PointOfSale
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void AddAngryChickenButtonClicked(object sender, RoutedEventArgs e)
+        void OnAddAngryChickenButtonClicked(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new AngryChicken());
         }
+
+        /// <summary>
+        /// Event handler for AddChiliCheeseFriesButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddChiliCheeseFriesButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new ChiliCheeseFries());
+        }
+
+        /// <summary>
+        /// Event handler for AddCornDodgersButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddCornDodgersButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new CornDodgers());
+        }
+
+        /// <summary>
+        /// Event handler for AddPanDeCampoButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddPanDeCampoButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new PanDeCampo());
+        }
+
+        /// <summary>
+        /// Event handler for AddBakedBeansButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddBakedBeansButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new BakedBeans());
+        }
+
+        /// <summary>
+        /// Event handler for AddJerkedSodaButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddJerkedSodaButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new JerkedSoda());
+        }
+
+        /// <summary>
+        /// Event handler for AddTexasTeaButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddTexasTeaButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new TexasTea());
+        }
+
+        /// <summary>
+        /// Event handler for AddCowboyCoffeeButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddCowboyCoffeeButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new CowboyCoffee());
+        }
+
+        /// <summary>
+        /// Event handler for AddWaterButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnAddWaterButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new Water());
+        }
+
     }
 }
