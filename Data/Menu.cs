9930d9f50ade502225c8972777afb9f845a8194e
Tristan Larson <telarson@ksu.edu>
2020-04-17 21:39:39 -0500

    Created the menu class.

---
+++ Data/Menu.cs
@@ -0,0 +1,17 @@
+﻿/* Menu.cs
+ * Author: Tristan Larson
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// The Menu class represents the menu of items for the Cowboy Cafe.
+    /// </summary>
+    public static class Menu
+    {
+
+    }
+}
