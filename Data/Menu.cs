fc722438517a112153c13ea303cc57c6f211e092
Tristan Larson <telarson@ksu.edu>
2020-04-18 00:10:11 -0500

    Added the static methods to the Menu class.

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -1,5 +1,6 @@
 ﻿/* Menu.cs
  * Author: Tristan Larson
+ * 
  */
 using System;
 using System.Collections.Generic;
@@ -13,5 +14,85 @@ namespace CowboyCafe.Data
     public static class Menu
     {
 
+        /// <summary>
+        /// Method that gets an IEnumerable of all entrees.
+        /// </summary>
+        /// <returns>An IEnumerable containing an instance of each entree class</returns>
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            IEnumerable<IOrderItem> entreeEnumerable = new Entree[]
+            {
+                new AngryChicken(),
+                new CowpokeChili(),
+                new DakotaDoubleBurger(),
+                new PecosPulledPork(),
+                new RustlersRibs(),
+                new TexasTripleBurger(),
+                new TrailBurger()
+            };
+
+            return entreeEnumerable;
+        }
+
+        /// <summary>
+        /// Method that gets an IEnumerable of all sides.
+        /// </summary>
+        /// <returns>An IEnumerable containing an instance of each side class</returns>
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            IEnumerable<IOrderItem> sideEnumerable = new Side[]
+            {
+                new BakedBeans(),
+                new ChiliCheeseFries(),
+                new CornDodgers(),
+                new PanDeCampo()
+            };
+            return sideEnumerable;
+        }
+
+        /// <summary>
+        /// Method that gets an IEnumerable of all drinks.
+        /// </summary>
+        /// <returns>An IEnumerable containing an instance of each drink class</returns>
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            IEnumerable<IOrderItem> drinkEnumerable = new Drink[]
+            {
+                new CowboyCoffee(),
+                new JerkedSoda(),
+                new TexasTea(),
+                new Water()
+            };
+
+            return drinkEnumerable;
+        }
+
+        /// <summary>
+        /// Method that gets an IEnumerable of all items.
+        /// </summary>
+        /// <returns>An IEnumerable containing an instance of each IOrderItem class</returns>
+        public static IEnumerable<IOrderItem> CompleteMenu()
+        {
+            IEnumerable<IOrderItem> menuEnumerable = new IOrderItem[]
+            {
+                new AngryChicken(),
+                new CowpokeChili(),
+                new DakotaDoubleBurger(),
+                new PecosPulledPork(),
+                new RustlersRibs(),
+                new TexasTripleBurger(),
+                new TrailBurger(),
+                new BakedBeans(),
+                new ChiliCheeseFries(),
+                new CornDodgers(),
+                new PanDeCampo(),
+                new CowboyCoffee(),
+                new JerkedSoda(),
+                new TexasTea(),
+                new Water()
+            };
+
+            return menuEnumerable;
+        }
     }
 }
